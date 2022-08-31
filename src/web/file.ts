aba2fbb6878112061b0984ee128488f2c40bfb1b
yhuijune <yanhuijun1024@163.com>
2022-08-31 22:08:08 +0800

    feature: to DataUrl

--- src/web/file.ts
+++ src/web/file.ts
@@ -3,22 +3,6 @@
  */
 
 /**
- * @description 将file|blob|stream 转 DataURL
- */
-export const blobToDataURL = (blob: Blob) => {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(blob);
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-        reader.onerror = (error) => {
-            reject(error);
-        };
-    });
-};
-
-/**
  * @description 下载文件（通过a标签）
  */
 export const download = ({
@@ -47,3 +31,61 @@ export const download = ({
 export const downloadByOpen = (url: string | URL) => {
     window.open(url);
 };
+
+/**
+ * @description 将file|blob|stream 转 DataURL
+ */
+export const blobToDataURL = (blob: Blob) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = (error) => {
+            reject(error);
+        };
+    });
+};
+
+/**
+ * @description 图片URL 转 DataUrl
+ */
+type ImageType = 'png' | 'gif' | 'jpg' | 'jpeg' | 'svg'
+
+export const getImgBase64 = (url: string, type: ImageType) => {
+    const image = new Image();
+    image.src = url;
+    return new Promise((resolve, reject) => {
+        image.onload = () => {
+            let mimetype;
+            switch (type) {
+                case 'svg': {
+                    mimetype = 'image/svg+xml'
+                    break;
+                }
+                case 'png':
+                case 'gif':
+                case 'jpg':
+                case 'jpeg': {
+                    mimetype = `${image}/${type}`
+                    break;
+                }
+                default: {
+                    mimetype = 'image/png'
+                }
+            }
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d")
+            const { width, height } = image
+            canvas.width = width;
+            canvas.height = height;
+            ctx?.drawImage(image, 0, 0, width, height);
+            const dataUrl = canvas.toDataURL(mimetype)
+            resolve(dataUrl)
+        }
+        image.onerror = (evt) => {
+            reject(evt)
+        }
+    })
+}
